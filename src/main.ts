e45ea1adea9fb21453597ac0fb44caafb04d3e06
bylkode <hafidsaadibk@gmail.com>
2022-11-15 06:50:27 -0500

    Add Wallet class

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,14 @@
 import './style.css';
+import Chain from './chain';
+import Wallet from './wallet';
 
+
+const satoshi = new Wallet();
+const bob = new Wallet();
+const alice = new Wallet();
+
+satoshi.sendMoney(50, bob.publicKey);
+bob.sendMoney(23, alice.publicKey);
+alice.sendMoney(5, bob.publicKey);
+
+console.log(Chain.instance);
